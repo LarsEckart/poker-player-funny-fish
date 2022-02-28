3a3b9d3dceb8c6f54dba1cdcc1937a3025ca7c3e
LarsEckart <lars.eckart@hey.com>
2022-02-28 21:44:27 +0200

    change version name

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,7 +4,7 @@ import com.google.gson.JsonElement;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "Something Fishy";
 
     public static int betRequest(JsonElement request) {
         return 100;
